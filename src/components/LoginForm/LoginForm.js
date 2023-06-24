cdaa515bf2f8cb3eb53d24f9bc54323a9c0625b4
ROXANA291088 <63433371+ROXANA291088@users.noreply.github.com>
2023-06-24 17:33:24 -0300

    validaciones

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,28 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const LoginForm = () => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const[user, setUser] = useState('');
-    const[password, setPassword] = useState('');
+  const loginHandler = (event) => {
+    event.preventDefault(); // Evita el envío del formulario por defecto
 
-    const loginHandler = () => {
-
+    if (user.trim() === '') {
+      setError('Ingrese un nombre de usuario válido.');
+      return;
     }
-    
-    return (
-        <div className='container'>
-        <h2>Login</h2>
-            <form className='form-group' onSubmit={loginHandler}>
-                    <label> Nombre de usuario </label>
-                    <input type="text" value={user} />
-                <br />
-                    <label> Contraseña </label>
-                    <input type="password" value={password} />
-                <br />
-                <button className='form-btn'>Ingresar</button>
-            </form>
-        </div>
-    )
-}
 
-export default LoginForm
+    if (password.trim() === '') {
+      setError('Ingrese una contraseña válida.');
+      return;
+    }
+
+    // Si todas las validaciones pasan, puedes realizar alguna acción adicional aquí, como enviar los datos al servidor.
+
+    // Reiniciar los campos y el estado de error después del envío exitoso
+    setUser('');
+    setPassword('');
+    setError('');
+  };
+
+  const userChangeHandler = (event) => {
+    setUser(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div className='container'>
+      <h2>Login</h2>
+      <form className='form-group' onSubmit={loginHandler}>
+        <label>Nombre de usuario</label>
+        <input type='text' value={user} onChange={userChangeHandler} />
+        <br />
+        <label>Contraseña</label>
+        <input type='password' value={password} onChange={passwordChangeHandler} />
+        <br />
+        <button className='form-btn'>Ingresar</button>
+      </form>
+      {error && <span>{error}</span>}
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/components/SingUpForm/SingUpForm.js
+++ src/components/SingUpForm/SingUpForm.js
@@ -1,48 +1,69 @@
-import { useState } from "react"
-
+import React, { useState } from "react";
 
 const SingUpForm = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
-    const[user, setUser] = useState('');
-    const[password, setPassword] = useState('');
-    const[email, setEmail] = useState('');
-    const[error, setError] = useState('');
+  const userChangeHandler = (event) => {
+    setUser(event.target.value);
+  };
 
-    const userChangeHandler = (event) => {
-        setUser(event.target.value);
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+
+    if (user.trim() === "") {
+      setError("Ingrese un nombre de usuario válido.");
+      return;
     }
-    const emailChangeHandler = (event) => {
-        setEmail(event.target.value);
-    }
-    const passwordChangeHandler = (event) => {
-        setPassword(event.target.value);
-    }
 
-    const addUserHandler = () => {
-
+    if (email.trim() === "") {
+      setError("Ingrese un correo electrónico válido.");
+      return;
     }
 
+    if (password.trim() === "") {
+      setError("Ingrese una contraseña válida.");
+      return;
+    }
 
-    return (
-        <div className='container'>
-        <h2>Formulario de Registro</h2>
-        <hr />
-        <form className='form-group'  onSubmit={addUserHandler}>
-    
-            <label>Usuario</label>
-            <input type="text" value={user} onChange={userChangeHandler} />
-         <br />
-            <label>Email</label>
-            <input type="email" value={email} onChange={emailChangeHandler}/>
-          <br />
-            <label>Contraseña</label>
-            <input type="password" value={password} onChange={passwordChangeHandler}/>
-          <br />
-          <button className='form-btn'>Registrarse</button>
-        </form>
-        {error && <span>{error}</span>}
-      </div>
-    )
-  }
+    // Si todas las validaciones pasan, puedes realizar alguna acción adicional aquí, como enviar los datos al servidor.
 
-export default SingUpForm
+    // Reiniciar los campos y el estado de error después del envío exitoso
+    setUser("");
+    setPassword("");
+    setEmail("");
+    setError("");
+  };
+
+  return (
+    <div className="container">
+      <h2>Formulario de Registro</h2>
+      <hr />
+      <form className="form-group" onSubmit={addUserHandler}>
+        <label>Usuario</label>
+        <input type="text" value={user} onChange={userChangeHandler} />
+        <br />
+        <label>Email</label>
+        <input type="email" value={email} onChange={emailChangeHandler} />
+        <br />
+        <label>Contraseña</label>
+        <input type="password" value={password} onChange={passwordChangeHandler} />
+        <br />
+        <button className="form-btn">Registrarse</button>
+      </form>
+      {error && <span>{error}</span>}
+    </div>
+  );
+};
+
+export default SingUpForm;
